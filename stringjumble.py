688ce77ec8f59c8fd5082f83bbcab76fdc53e5f6
Spyglass117 <46973734+Spyglass117@users.noreply.github.com>
2019-03-19 14:02:17 -0400

    Updated from Brython Server: 3/19/2019 2:02:16 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,19 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+str1 = input("Please enter a string of text (the bigger the better): ")
+print ("You entered {0}. Now jumble it:".format(str1))
+
+
+str2 = str1[::-1]
+
+
+list1 = str1.split()
+list2 = list1[::-1]
+
+str3 = " ".join(list2)
+
+
+
+print (str2)
+print(str3)
